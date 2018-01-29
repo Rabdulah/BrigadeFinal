87091840f1404351385046ea7e0ce0371a49a397
Omar-Chahbar <ochahba@uwo.ca>
2018-01-28 22:35:21 -0500

    added model for assessmentTest

---
+++ SelfStartBackend/models/assessmentsTest.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var assessmentTestSchema = mongoose.Schema({
+    name: String,
+    description: String,
+    authorName: String,
+    testResult: {
+                    question: String,
+                    answer: String
+                },
+    rehabilitationPlan: {type: mongoose.Schema.ObjectId, ref: 'RehabilitationPlans'},
+    assessmentTool: {type: mongoose.Schema.ObjectId, ref: 'Forms'},
+    recommendation: [{type: mongoose.Schema.ObjectId, ref: 'Recommendations'}]
+
+});
+var AssessmentTestSchema = mongoose.model('assessmentTest', assessmentTestSchema);
+exports.Model = AssessmentTestSchema;
