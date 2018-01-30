fa77b7bd1e521c1620f7d87f45e49289d365c756
youda97 <youda@uwo.ca>
2018-01-30 00:56:15 -0500

    Lab 1 Deliverab;es

---
+++ BackEnd/models/RehabilitationPlans.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var rehabilitationPlansSchema = mongoose.Schema({
+    planName: String,
+    description: String,
+    physioID: String,
+    goal: String,
+    date: Date,
+    timeToComplete: String,
+    plan: [{type: mongoose.Schema.ObjectId, ref: 'Treatments'}],
+    test: [{type: mongoose.Schema.ObjectId, ref: 'AssessmentTests'}],
+    exercise: [{type: mongoose.Schema.ObjectId, ref: 'Exercises'}]
+
+});
+var RehabilitationPlansSchema = mongoose.model('rehabilitationPlan', rehabilitationPlansSchema);
+exports.Model = RehabilitationPlansSchema;
---
+++ BackEnd/routes/rehabilitationplans.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var router = express.Router();
+var Rehabilitation = require('../models/RehabilitationPlans');
+
+router.route('/')
+    .post( function (request, response) {
+        var rehabilitation = new Rehabilitation.Model(request.body.rehabilitation);
+        rehabilitation.save(function (error) {
+            if (error) response.send(error);
+            response.json({rehabilitation: rehabilitation});
+        });
+    })
+    .get( function (request, response) {
+        Rehabilitation.Model.find(function (error, rehabilitations) {
+            if (error) response.send(error);
+            response.json({rehabilitation: rehabilitations});
+        });
+    });
+    
+router.route('/:rehabilitation_id')
+    .get( function (request, response) {
+        Rehabilitation.Model.findById(request.params.rehabilitations_id, function (error, rehabilitations) {
+            if (error) {
+                response.send({error: error});
+            }
+            else {
+                response.json({rehabilitation: rehabilitations});
+            }
+        });
+    })
+   
+    .put( function (request, response) {
+        Rehabilitation.Model.findById(request.params.exer_id, function (error, rehabilitation) {
+            if (error) {
+                response.send({error: error});
+            }
+            else {
+
+                // update each attribute
+                rehabilitation.planName = request.body.rehabilitation.planName;
+                rehabilitation.description = request.body.rehabilitation.description;
+                rehabilitation.physioID = request.body.rehabilitation.physioID;
+                rehabilitation.goal = request.body.rehabilitation.goal;
+                rehabilitation.date = request.body.rehabilitation.date;
+                rehabilitation.timeToComplete = request.body.rehabilitation.timeToComplete;
+                rehabilitation.plan = request.body.rehabilitation.plan;
+                rehabilitation.test = request.body.rehabilitation.test;
+                rehabilitation.exercise = request.body.rehabilitation.exercise;
+
+
+                rehabilitation.save(function (error) {
+                    if (error) {
+                        response.send({error: error});
+                    }
+                    else {
+                        response.json({rehabilitation: rehabilitation});
+                    }
+                });
+            }
+        });
+    })
+    
+    .delete( function (request, response) {
+        Rehabilitation.Model.findByIdAndRemove(request.params.exercise_id,
+            function (error, deleted) {
+                if (!error) {
+                    response.json({rehabilitation: deleted});
+                }
+            }
+        );
+    });
+
+module.exports = router;
