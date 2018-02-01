0f1753d80547af7cff05246964cf9e938c9aa43c
Jeffrey(Seung Jun) Kim <you7046@gmail.com>
2018-02-01 18:38:34 -0500

    made routes and other

---
+++ SelfStart/SelfStartFrontEnd/app/components/add-rehabplan.js
@@ -0,0 +1,37 @@
+import Component from '@ember/component';
+import { inject } from '@ember/service';
+
+export default Component.extend({
+  DS: inject('store'),
+
+  isEditing: false,
+
+  actions: {
+    addPatient (){
+      this.set('isEditing', true);
+    },
+
+    cancel: function () {
+      this.set('isEditing', false);
+    },
+
+    submit: function () {
+
+      let self = this;
+
+      let patient = this.get('DS').createRecord('patient', {
+        familyName: self.get('familyName'),
+        givenName: self.get('givenName'),
+        email: self.get('email'),
+      });
+
+      patient.save().then(function() {
+        this.set('isEditing', false);
+      });
+      this.set('familyName', '');
+      this.set('givenName', '');
+      this.set('email', '');
+    }
+  },
+
+});
---
+++ SelfStart/SelfStartFrontEnd/app/models/rehabplan.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+
+  name:DS.attr(),
+  description: DS.attr(),
+  authorName: DS.attr(),
+  goal : DS.attr(),
+  timeFrameToComplete: DS.attr(),
+  exercises : DS.hasMany('exercise', { async: true }),
+  assessmentTests : DS.hasMany ('assesmentTest', { async: true })
+
+});
