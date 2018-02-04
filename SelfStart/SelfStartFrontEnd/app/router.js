53ff4d940429cd5d0d28357b2f1b8d7e5001eb4d
Ramzi Abdulahi <ramzi_abdullahi@outlook.com>
2018-02-04 17:37:56 -0500

    Revert "Merge branch 'master' into jk"
    This reverts commit 9b03a2630a2c716787ff99dd9c2e631379a7c972, reversing
    changes made to b6ce3d3b0046f5636d9f3d5d88185d819dd7c5bb.

--- SelfStart/SelfStartFrontEnd/app/router.js
+++ SelfStart/SelfStartFrontEnd/app/router.js
@@ -9,11 +9,7 @@ const Router = EmberRouter.extend({
 Router.map(function() {
   this.route('home', {path: '/'});
   this.route('patients');
-<<<<<<< HEAD
   this.route('rehabplans');
-=======
-  this.route('admin');
->>>>>>> master
 });
 
 export default Router;
