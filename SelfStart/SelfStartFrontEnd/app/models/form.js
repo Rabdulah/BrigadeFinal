d266dd3768e18f411ae48e89eb8ce759f4f5fbd3
Brandon D <bdicicco@uwo.ca>
2018-02-09 17:59:38 -0500

    Manage form created

--- SelfStart/SelfStartFrontEnd/app/models/form.js
+++ SelfStart/SelfStartFrontEnd/app/models/form.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     name: DS.attr(),
     description: DS.attr(),
-    author: DS.attr()
+    author: DS.attr(),
+    question: [DS.attr()]
 });
