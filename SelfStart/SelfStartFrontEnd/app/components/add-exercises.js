f8a6f08b82d3643a4a1eefdfecf6e41fd85f97a2
Mustafa Dawoud <mustafadawoud97@gmail.com>
2018-02-11 13:57:35 -0500

    Exercise

---
+++ SelfStart/SelfStartFrontEnd/app/components/add-exercises.js
@@ -0,0 +1,45 @@
+import Component from '@ember/component';
+import { inject } from '@ember/service';
+import $ from 'jquery';
+
+export default Component.extend({
+    DS: inject('store'),
+
+    actions: {
+        toggle: function(){
+            $('.ui.dropdown').dropdown({
+                // transition: 'horizontal flip',
+                on: 'hover'
+            });
+        },
+
+        submit: function() {
+            let exercise = this.get('DS').createRecord('exercise', {
+                name:this.get('Name'),
+                description:this.get('Description'),
+                objectives:this.get('Objective'),
+                authorName:this.get('AuthName'),
+                actionSteps:this.get('ActionSteps'),
+                location:this.get('Location'),
+                frequency:this.get('Frequency'),
+                duration:this.get('Duration'),
+                multimediaURL:this.get('MMURL'),
+                targetDate:this.get('TargetDate')
+                //rehabilitationPlan:this.get('rehabPlan'),
+            });
+
+            exercise.save().then(function(){});
+                // this.set('Name', "");
+                // this.set('Description', "");
+                // this.set('Objective', "");
+                // this.set('AuthName', "");
+                // this.set('ActionStep', "");
+                // this.set('Location', "");
+                // this.set('Frequency', "");
+                // this.set('Duration', "");
+                // this.set('MMURL', "");
+                // this.set('TargetDate', "");
+            //});
+        }   
+    }
+});
--- SelfStart/SelfStartFrontEnd/app/router.js
+++ SelfStart/SelfStartFrontEnd/app/router.js
@@ -9,6 +9,7 @@ const Router = EmberRouter.extend({
 Router.map(function() {
   this.route('home', {path: '/'});
   this.route('patients');
+  this.route('exercises');
 });
 
 export default Router;
