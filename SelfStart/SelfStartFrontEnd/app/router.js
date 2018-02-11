c6d52cd715ea11f65112d05cfb4afb4f7bbfeaba
Ramzi Abdulahi <ramzi_abdullahi@outlook.com>
2018-02-11 16:49:34 -0500

    Hope

--- SelfStart/SelfStartFrontEnd/app/router.js
+++ SelfStart/SelfStartFrontEnd/app/router.js
@@ -9,17 +9,11 @@ const Router = EmberRouter.extend({
 Router.map(function() {
   this.route('home', {path: '/'});
   this.route('patients');
-<<<<<<< HEAD
+
   this.route('rehab-form');
-=======
-  this.route('rehabplans');
-<<<<<<< HEAD
->>>>>>> jk
-=======
   this.route('admin');
   this.route('questions');
   this.route('forms');
->>>>>>> jk
 });
 
 export default Router;
