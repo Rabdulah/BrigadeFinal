2f06cd8a9a8ed6b4d9837c7383c52c1edccd10e9
Mustafa Dawoud <mustafadawoud97@gmail.com>
2018-02-20 02:49:54 -0500

    Commiting before changing models

--- SelfStart/SelfStartFrontEnd/app/components/add-exercises.js
+++ SelfStart/SelfStartFrontEnd/app/components/add-exercises.js
@@ -2,22 +2,99 @@ import Component from '@ember/component';
 // import { computed } from '@ember/object';
 import { inject } from '@ember/service';
 import Ember from 'ember';
+import fileObject from "../utils/file-object";
 
 export default Component.extend({
     DS: inject('store'),
 
+    // ImageName: null,
+    model: 'image',
+    flag: null,
+    accept: 'audio/*,video/*,image/*',
+    multiple: true,
+    queue: [],
+    savingInProgress: false,
     isEditing: false,
-
     id: null,
+    
+    labelArray: [
+        'height: 6.25em',
+        'line-height: 5.25em',
+        'text-align: center',
+    ],
+    
+    inputStyle: Ember.computed(function () {
+        let style_array = [
+          'opacity: 0',
+          'width:100% !important',
+          'overflow:hidden',
+          'position:relative',
+          'left:-100%',
+          'display:block',
+        ];
+        return Ember.String.htmlSafe(style_array.join(';'));
+    }),
+    
+    labelStyle: Ember.computed('labelArray', function () {
+        return Ember.String.htmlSafe(this.get('labelArray').join(';'));
+    }),
+    
+    dragLeave: function (event) {
+        event.preventDefault();
+        this.set('labelArray', [
+          'height: 6.25em',
+          'line-height: 5.25em',
+          'text-align: center',
+        ]);
+        return this.set('dragClass', 'deactivated');
+    },
+    
+    dragOver: function () {
+        this.set('labelArray', [
+          'height: 6.25em',
+          'line-height: 5.25em',
+          'text-align: center',
+          'background: green',
+        ]);
+    },
+    
+    drop: function () {
+        this.set('labelArray', [
+          'height: 6.25em',
+          'line-height: 5.25em',
+          'text-align: center',
+        ]);
+    },
 
     obj: [],
-
-    // exerciseData: null,
 
     actionStep: [],
 
     actions: {
-       
+        selectFile: function (data) {
+            if (!Ember.isEmpty(data.target.files)) {
+                for (let i = data.target.files.length - 1; i >= 0; i--) {
+                    let file = fileObject.create({
+                    fileToUpload: data.target.files[i],
+                    maximumFileSize: 6
+                    });
+                this.get('queue').pushObject(file);
+              }
+            }
+        },
+        
+        deleteFile: function (file) {
+            this.get('queue').removeObject(file);
+            if (Ember.isEmpty(this.get('queue'))) {
+              this.set('flag', false);
+            }
+        },
+
+        done: function () {
+            this.get('queue').clear();
+            this.set('flag', false);
+        },
+
         addActionStep(){
             let newActStep = this.get('ActionSteps');
             this.get('actionStep').pushObject(newActStep);
@@ -93,11 +170,14 @@ export default Component.extend({
                 frequency:this.get('Frequency'),
                 duration:this.get('Duration'),
                 multimediaURL:this.get('MMURL'),
-                targetDate:this.get('TargetDate')
+                targetDate:this.get('TargetDate'),
+                image: this.get('queue')
                 //rehabilitationPlan:this.get('rehabPlan'),
             });
 
-            exercise.save().then(function(){});
+            exercise.save().then(function(){
+
+            });
             this.set('Name', "");
             this.set('Description', "");
             this.set('Objective', "");
